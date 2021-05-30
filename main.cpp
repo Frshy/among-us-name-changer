3ca9bae7d646520c7060f65eba28f2b33dc9d074
Freshy <79219288+Frshy@users.noreply.github.com>
2021-05-30 14:47:22 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <Windows.h>
+
+using namespace std;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void throwError(string error) {
+	SetConsoleTextAttribute(hConsole, 12);
+	printf("[-] Error: %s\n", error.c_str());
+}
+
+int main() {
+
+	//settings up path
+	string username = getenv("USERNAME");
+	ostringstream amongusConfigPath;
+	amongusConfigPath << "C:\\Users\\" << username << "\\AppData\\LocalLow\\Innersloth\\Among Us\\playerPrefs";
+
+	//reading config
+	ifstream configFile;
+	configFile.open(amongusConfigPath.str());
+	if (!configFile.good()) {
+		SetConsoleTextAttribute(hConsole, 12);
+		throwError("among us config file not found!");
+		system("pause");
+		return 0;
+	}
+	stringstream strStream;
+	strStream << configFile.rdbuf();
+	configFile.close();
+	string config = strStream.str();
+	
+	//asking user for new name
+	string newName;
+	SetConsoleTextAttribute(hConsole, 14);
+	printf("[?] Enter name you want to change to > ");
+	cin >> newName;
+
+	//replaceing name in readen file to new name
+	string actualName = config.substr(0, config.find(","));
+	config.replace(0, actualName.size(), newName);
+
+	//writing file with new name
+	ofstream newConfig;
+	newConfig.open(amongusConfigPath.str());
+	if(!newConfig.good()){
+		SetConsoleTextAttribute(hConsole, 12);
+		throwError("can't write the file!");
+		system("pause");
+		return 0;
+	}
+	newConfig << config;
+	newConfig.close();
+
+	//you receive this message if everything went good
+	SetConsoleTextAttribute(hConsole, 10);
+	printf("[+] Changed nickname \"%s\" -> \"%s\" \n", actualName.c_str(), newName.c_str());
+	system("pause");
+	return 0;
+}
